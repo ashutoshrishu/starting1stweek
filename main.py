b4e623a8f358aaafd19236562bf0ae44ee90ecfd
ASHUTOSH KUMAR <83541324+ashutoshrishu@users.noreply.github.com>
2021-05-25 15:18:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,55 @@
+# num=13
+# print(type(num))
+
+
+# n = int(input())
+# sum = 2
+# for i in range(n+1):
+#     sum+=i
+#     print(sum)
+
+
+# sum = 0
+# i =0
+# while(i<=5):
+#     sum+=i
+#     i+=1
+#     print(sum)
+
+
+# sum = 0
+# for i in range(1,6):
+#     cube = i**3
+#     sum+= cube
+#     print(sum)
+
+
+# sum = 0
+# i = 0
+# while i<=5:
+#     sum +=i
+#     i+= 1
+#     print(sum)
+
+
+# for i in range(2,56):
+#     if (i==43):
+#         break
+#     print(i)
+
+
+# i = 0
+# while i<=26:
+#     if(i==17):
+#         break
+#     i+= 1
+#     print(i)
+
+
+# ls = ["ashu",25,True,20.9]
+# print(ls)
+# print(type(ls))
+
+# ls = [1,3,5,37,85,847,0,73,9]
+# for i in ls:
+#     print(ls)
